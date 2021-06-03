e1e920769ab93761f9b2d559be0143ce1f03f639
Har3366 <62189272+Har3366@users.noreply.github.com>
2021-06-04 00:38:41 +0300

    Add files via upload

--- ColorCircles/ColorCircles/ActionFunctions.swift
+++ ColorCircles/ColorCircles/ActionFunctions.swift
@@ -14,17 +14,42 @@ func circleMoving(_ gesture:UIPanGestureRecognizer){
 func circleMerge(_ circle1:UIView,with circle2:UIView,_ mergedColor:UIColor){
     if circle1.isHidden == true || circle2.isHidden == true {return}
     let circleFrame = circle2.frame
-    let circle2width = circle1.frame.width >= circle2.frame.width ? circle1.frame.width + 40 : circle2.frame.width + 40
-    let circle2height = circle1.frame.height >= circle2.frame.height ? circle1.frame.height + 40 : circle2.frame.height + 40
+    let increaseValue: CGFloat = mergedColor == UIColor(#colorLiteral(red: 0.6035447121, green: 0, blue: 0.2712310255, alpha: 1)) ? 50 : 40
+    let circle2width = circle1.frame.width >= circle2.frame.width ? circle1.frame.width + increaseValue : circle2.frame.width + increaseValue
+    let circle2height = circle1.frame.height >= circle2.frame.height ? circle1.frame.height + increaseValue : circle2.frame.height + increaseValue
     for coordX in Int(circleFrame.minX) ... Int(circleFrame.maxX) {
         for coordY in Int(circleFrame.minY) ... Int(circleFrame.maxY){
             if coordY == Int(circle1.center.y) && coordX == Int(circle1.center.x) {
                 circle1.isHidden = true
-                circle2.backgroundColor = mergedColor
-                circle2.frame.size.width = circle2width
-                circle2.frame.size.height = circle2height
-                circle2.layer.cornerRadius = circle2.frame.height/2
-                circle2.reloadInputViews()
+                
+//                UIView.animate(withDuration: 1, delay: 0, options: .curveLinear) {
+//                    circle2.backgroundColor = mergedColor
+//                    circle2.frame.size.width = circle2width
+//                    circle2.frame.size.height = circle2height
+//                    circle2.layer.cornerRadius = circle2.frame.height/2
+//                    circle2.reloadInputViews()
+//                }
+                UIView.animateKeyframes(withDuration: 2, delay: 0, options:.calculationModeCubic) {
+                    UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5) {
+                        
+                        circle2.bounds.size.width = circle2width
+                        circle2.bounds.size.height = circle2height
+                        circle2.layer.cornerRadius = circle2.frame.height/2
+                        circle2.reloadInputViews()
+                    }
+                    UIView.addKeyframe(withRelativeStartTime: 0.3, relativeDuration: 0.7) {
+                        circle2.backgroundColor = mergedColor
+                    }
+                }
+
+
+                
+                
+                
+                
+                
+                
+                
             }
         }
     }
